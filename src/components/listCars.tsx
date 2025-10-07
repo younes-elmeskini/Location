6bccaea3a047b9b28d7a2b686196c1bbb28ff0e3
Elmeskini Youness <yns.elmeskini@gmail.com>
2025-10-08 00:11:08 +0100

    Ajout de la dépendance "react-icons" dans package.json et package-lock.json. Modification du composant ListCars pour utiliser les nouveaux composants CarCardAdmin et ActionButtons, améliorant ainsi l'affichage et la gestion des voitures.

--- src/components/listCars.tsx
+++ src/components/listCars.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import CarCardAdmin from "./carCardAdmin";
+import ActionButtons from "@/UI/actionButtons";
 
 // Define the type for each car
 type Car = {
-  id: number;
+  id: string;
   name: string;
   type: string;
   image?: string;
@@ -11,6 +13,7 @@ type Car = {
   price: string;
   seats: number;
   dors: number;
+  quantity:number;
   transmission: string;
   fuelType: string;
   airConditioning: boolean;
@@ -63,53 +66,11 @@ export default function ListCars({ filter }: ListCarsProps) {
   if (error) return <p className="text-red-500">Error: {error}</p>;
 
   return (
-    <table className="min-w-full border border-gray-300 border-collapse">
-      <thead className="bg-gray-100">
-        <tr>
-          <th className="px-4 py-2 border">Name</th>
-          <th className="px-4 py-2 border">Type</th>
-          <th className="px-4 py-2 border">Price</th>
-          <th className="px-4 py-2 border">Seats</th>
-          <th className="px-4 py-2 border">Doors</th>
-          <th className="px-4 py-2 border">Transmission</th>
-          <th className="px-4 py-2 border">Fuel</th>
-          <th className="px-4 py-2 border">AC</th>
-          <th className="px-4 py-2 border">Cover</th>
-          <th className="px-4 py-2 border">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {cars.map((car, index) => (
-          <tr key={car.id} className={`${index % 2 === 0 ? 'bg-white' : 'bg-gray-50'} hover:bg-gray-200 text-center`}>
-            <td className="px-4 py-2 border">{car.name}</td>
-            <td className="px-4 py-2 border">{car.type}</td>
-            <td className="px-4 py-2 border">{car.price}</td>
-            <td className="px-4 py-2 border">{car.seats}</td>
-            <td className="px-4 py-2 border">{car.dors}</td>
-            <td className="px-4 py-2 border">{car.transmission}</td>
-            <td className="px-4 py-2 border">{car.fuelType}</td>
-            <td className="px-4 py-2 border">{car.airConditioning ? "Yes" : "No"}</td>
-            <td className="px-4 py-2 border">
-              {car.cover && <Image src={car.cover} alt={car.name} className="h-12 mx-auto" width={100} height={100} />}
-            </td>
-            <td className="px-4 py-2 border space-x-2">
-              <button
-                className="px-2 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
-                onClick={() => (window.location.href = `/admin/${car.id}`)} 
-                
-              >
-                Edit
-              </button>
-              <button
-                className="px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600"
-                onClick={() => handleDelete(car)}
-              >
-                Delete
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:gap-8 gap-6 my-6">
+    {cars.length === 0 && <p className="text-center">No cars available.</p>}
+    {cars.map((car) => (
+      <CarCardAdmin key={car.id} {...car} />
+    ))}
+  </div>
   );
 }
