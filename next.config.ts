5f1b32c4d061d4ee6783dd6f03f205ac64766c96
Elmeskini Youness <yns.elmeskini@gmail.com>
2025-10-08 05:21:34 +0100

    Refactor Next.js configuration to use TypeScript for type safety and enhance image handling by adding remote patterns for Cloudinary.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
