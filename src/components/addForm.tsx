8191654ac5852a32ac0d2c2eb25e6ffbed7431bc
Elmeskini Youness <yns.elmeskini@gmail.com>
2025-10-08 05:38:48 +0100

    Refactor addForm component by removing unused import of useEffect, streamlining the code for better readability and maintainability.

--- src/components/addForm.tsx
+++ src/components/addForm.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Image from "next/image";
-
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import {
   CarType,
